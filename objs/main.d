820af7dde6647c9bec2c9117f860385c59661fc4
Namkyung Koo <nakoo@c4r6s5.42seoul.kr>
2023-02-03 21:03:57 +0900

    feat : Add env

--- objs/main.d
+++
@@ -1,2 +0,0 @@
-objs/main.o: srcs/main.c includes/minishell.h \
-  includes/../libft/includes/libft.h includes/parsing.h
--- objs/parsing.d
+++
@@ -1,2 +0,0 @@
-objs/parsing.o: srcs/parsing.c includes/parsing.h includes/minishell.h \
-  includes/../libft/includes/libft.h
