6904e31d788762d26c152fa4c5f3d6cadd819aa5
euiclee <lechan1317@gmail.com>
2023-02-03 13:17:01 +0900

    feat: add depen lalala

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,15 +6,20 @@
 /*   By: euiclee <euiclee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:32:05 by euiclee           #+#    #+#             */
-/*   Updated: 2023/02/01 19:26:58 by euiclee          ###   ########.fr       */
+/*   Updated: 2023/02/03 12:27:41 by euiclee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# define BLUE "\033[34;1m"
+# define RESET "\033[0m"
+
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 # include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
