bef986ca7c192b62a2bac3675c1fa2c3a4fada45
Namkyung Koo <nakoo@c4r7s4.42seoul.kr>
2023-02-07 14:39:21 +0900

    Add : echo

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 14:04:05 by nakoo             #+#    #+#             */
-/*   Updated: 2023/02/07 00:46:16 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/02/07 14:05:46 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,13 @@
 # define FALSE 0
 # define TRUE 1
 
+# define NL 0
+# define NO_NL 1
+
 void	ft_cd(char *path, char **env);
-void	ft_echo(char *str);
+void	ft_echo(char **token);
 void	ft_env(char **env);
 void	ft_pwd(char **envp);
+void	ft_exit(void);
 
 #endif
--- libft/srcs/ft_strcmp.c
+++ libft/srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 17:22:56 by nakoo             #+#    #+#             */
-/*   Updated: 2023/01/13 17:23:08 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/02/07 14:36:26 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strcmp(const char *s1, const char *s2)
 	int	i;
 
 	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (-2147483648);
 	while ((s1[i] != '\0' && s2[i] != '\0') && (s1[i] == s2[i]))
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
