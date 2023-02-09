5912b6b07b21fee996e7cc6f2b8727598014c0b6
Namkyung Koo <nakoo@c4r6s3.42seoul.kr>
2023-02-09 19:37:23 +0900

    Fix : cd.c

--- includes/execute.h
+++ includes/execute.h
@@ -1,31 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   execute.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/02 14:03:03 by nakoo             #+#    #+#             */
-/*   Updated: 2023/02/09 19:22:07 by nakoo            ###   ########.fr       */
+/*   Created: 2023/02/09 15:52:44 by nakoo             #+#    #+#             */
+/*   Updated: 2023/02/09 15:53:02 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "builtin.h"
+#ifndef EXECUTE_H
+# define EXECUTE_H
 
-void	ft_cd(char *path, char **env)
-{
-	char	*oldpwd;
-	char	*pwd;
-
-	oldpwd = ft_strfind(env, "OLDPWD=") + 7;
-	pwd = getcwd(NULL, 0);
-	ft_strlcpy(oldpwd, pwd, ft_strlen(pwd) + 1);
-	free(pwd);
-	if (*path == '\0')
-		pwd = ft_strfind(env, "HOME=") + 5;
-	else
-		pwd = path;
-	chdir(pwd);
-	ft_strlcpy(ft_strfind(env, "PWD=") + 4, pwd, ft_strlen(pwd) + 1);
-	free(path);
-}
+#endif
--- srcs/execute.c
+++ srcs/execute.c
@@ -1,31 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/02 14:03:03 by nakoo             #+#    #+#             */
-/*   Updated: 2023/02/09 19:22:07 by nakoo            ###   ########.fr       */
+/*   Created: 2023/02/09 15:50:52 by nakoo             #+#    #+#             */
+/*   Updated: 2023/02/09 17:11:26 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "builtin.h"
+#include "execute.h"
 
-void	ft_cd(char *path, char **env)
+/* token struct */
+typedef struct s_tokens {
+	char	**token;
+	int		*redirection;
+}	t_tokens;
+
+void	execute(t_tokens *tok, char **env)
 {
-	char	*oldpwd;
-	char	*pwd;
-
-	oldpwd = ft_strfind(env, "OLDPWD=") + 7;
-	pwd = getcwd(NULL, 0);
-	ft_strlcpy(oldpwd, pwd, ft_strlen(pwd) + 1);
-	free(pwd);
-	if (*path == '\0')
-		pwd = ft_strfind(env, "HOME=") + 5;
-	else
-		pwd = path;
-	chdir(pwd);
-	ft_strlcpy(ft_strfind(env, "PWD=") + 4, pwd, ft_strlen(pwd) + 1);
-	free(path);
+	
 }
