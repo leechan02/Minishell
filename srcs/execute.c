c2f9ff44bbe8a4a98215791d2a9bde891d4480ac
Namkyung Koo <nakoo@c4r6s3.42seoul.kr>
2023-02-09 19:47:43 +0900

    Add : execute comments

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,7 +6,7 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:50:52 by nakoo             #+#    #+#             */
-/*   Updated: 2023/02/09 17:11:26 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/02/09 19:47:23 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ typedef struct s_tokens {
 	int		*redirection;
 }	t_tokens;
 
-void	execute(t_tokens *tok, char **env)
+void	execute(t_tokens **tok, char **env)
 {
-	
+	/* pipe 기준으로 t_tokens가 들어온다. */
+	/* 1. token[i]부터 ->redirecion 처리, T/F도 판별 */
+	/* 2. cmd 판별(builtin or not), To handle error case */
+	/* 3. pipe ? */
+	/* 4. pipe 병렬 처리 */
 }
