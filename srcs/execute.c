2463636bc0d4408f08734c7b9b4b30b5b727a0fa
koonamkyung <united94@naver.com>
2023-02-12 16:50:36 +0900

    Think : execute section

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,7 +6,7 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:50:52 by nakoo             #+#    #+#             */
-/*   Updated: 2023/02/10 19:23:38 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/02/12 16:42:50 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,12 @@ int	execute(t_tokens *tokens, char **env)
 	int		fd[2];
 	int		i;
 	
-	/* default로 STDOUT을 pipe로 연결 */
-	/* 1. token[i]부터 ->redirecion 처리, T/F도 판별 */
-	/* 2. cmd 판별(builtin or not), To handle error case */
-	/* 3. pipe ? */
-	/* 4. pipe 병렬 처리 */
-	fd[0] = STDIN_FILENO;
-	fd[1] = STDOUT_FILENO:
+	/* 1. token이 1개보다 많다면(pipe를 기준으로 나눴기 때문) */
+	/* 2. default로 STDOUT을 pipe로 연결 */
+	/* 3. token[i]부터 ->redirecion 처리, T/F도 판별 */
+	/* 4. cmd 판별(builtin or not), To handle error case */
+	/* 5. pipe ? */
+	/* 6. pipe 병렬 처리 */
 	i = 1;
 	while (i < tokens)
 		pid = fork();
@@ -48,3 +47,12 @@ int	execute(t_tokens *tokens, char **env)
 	}
 	return (SUCCESS);
 }
+
+/* 기본적으로 동적 할당된 구조체 포인터가 들어온다.
+구조체 포인터의 멤버변수 이중 포인터에 접근해서 큰 반복문의 조건은 이중포인터로 
+작은 반복문의 조건은 이중 포인터 속 포인터로 접근 
+단순하게 생각해서 커맨드면 실행 시켜주고
+리다이렉션이면 입/출력 스트림을 변경시켜준다. 
+그리고나서 파이프에 넘겨준다. 
+Q1. 토큰이 하나여도 자식 프로세스 실행 ? (일관된 코드 || 반복문 조건 성립 x)
+*/
