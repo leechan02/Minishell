4eb1fd7017d55074cf370d60b7657153c54bae95
Namkyung Koo <nakoo@c4r6s2.42seoul.kr>
2023-02-14 15:17:58 +0900

    Fix : echo

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -6,30 +6,17 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 14:03:03 by nakoo             #+#    #+#             */
-/*   Updated: 2023/02/14 13:32:10 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/02/14 15:16:37 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-static int	is_redir(t_tokens *tokens, int i)
-{
-	int	fd;
-	int	j;
-
-	fd = 1;
-	j = 0;
-	while (tokens->token[i][j] != NULL)
-	{
-		if (ft_strcmp(tokens->token[i][j], "<") == 0 && tokens->redir[j] == TRUE)
-	}
-}
-
-static int	is_n(char *str)
+static int	is_n(char *str, int *nl)
 {
 	int	i;
 
-	i = 0;
+	i = 1;
 	while (str[i] != '\0')
 	{
 		if (str[i] != 'n')
@@ -40,32 +27,31 @@ static int	is_n(char *str)
 	return (TRUE);
 }
 
-int	ft_echo(t_tokens *tokens, int i)
+int	ft_echo(char **token)
 {
 	int	nl;
-	int	fd;
-	int	j;
+	int	i;
 
-	j = 0;
+	i = -1;
 	nl = NL;
-	fd = is_redir(tokens);
-	if (ft_strcmp(*(token + j), "-n") == 0)
+	while (ft_strcmp(token[++i], "echo") != 0)
+		;
+	if (token[++i] == NULL)
+		return (write (1, "\n", 1));
+	while (ft_strncmp(token[i], "-n", 2) == 0)
 	{
-		nl = NO_NL;
-		j++;
+		if (is_n(token[i], &nl) != TRUE)
+			break ;
+		if (token[++i] == NULL)
+			return (write (1, "", 0));
 	}
-	if (*(token + j) == NULL)
+	while (token[i] != NULL)
 	{
-		if (nl == NL)
-			return (ft_putchar_fd('\n', fd), TRUE);
-	}
-	while (*(token + j) != NULL)
-	{
-		ft_putstr_fd(*(token + j), fd);
-		/* if (마지막일 때, 공백 출력 x) */
-		ft_putchar_fd(' ', fd);
-		j++;
+		write (1, token[i], ft_strlen(token[i]));
+		if (token[++i] != NULL)
+			write (1, " ", 1);
 	}
 	if (nl != NO_NL)
-		return (ft_putchar_fd('\n', fd), TRUE);
+		write (1, "\n", 1);
+	return (TRUE);
 }
