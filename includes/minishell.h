49a791d0b75a54256380cf419b39bbf5549fc229
euiclee <lechan1317@gmail.com>
2023-02-14 14:00:18 +0900

    rm: minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: euiclee <euiclee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:32:05 by euiclee           #+#    #+#             */
-/*   Updated: 2023/02/13 13:17:18 by euiclee          ###   ########.fr       */
+/*   Updated: 2023/02/14 13:59:01 by euiclee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../libft/includes/libft.h"
-# include "builtin.h"
 
 # define TRUE 1
 # define FALSE 0
