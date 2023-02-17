ad95c9d55797d2021adc7d25c1ab73c8e0c32639
koonamkyung <united94@naver.com>
2023-02-17 19:24:42 +0900

    Fix : cd (not seg)

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 14:03:03 by nakoo             #+#    #+#             */
-/*   Updated: 2023/02/15 15:43:51 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/02/17 19:24:14 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	ft_cd(char **token, char **env)
 	while (ft_strcmp(token[++i], "cd") != 0)
 		;
 	oldpwd = ft_strfind(env, "OLDPWD=") + 7;
-	copy = ft_strdup(ft_strfind(env, "OLDPWD=") + 7);
+	copy = ft_strdup(oldpwd);
 	pwd = getcwd(NULL, 0);
 	ft_strlcpy(oldpwd, pwd, ft_strlen(pwd) + 1);
 	free(pwd);
@@ -41,12 +41,14 @@ int	ft_cd(char **token, char **env)
 		pwd = ft_strfind(env, "HOME=") + 5;
 	else if (token[i + 1][0] == '-')
 	{
-		printf("%s\n", copy);
-		return (free(copy), error_msg((chdir(copy) == 0), "cd "));
+		if (error_msg(chdir(copy) == 0, "cd ") == TRUE)
+			printf("%s\n", copy);
+		return (free(copy), TRUE);
 	}
 	else if (token[i + 1] != NULL)
 		pwd = token[i + 1];
-	error_msg((chdir(pwd) == 0), "cd ");
-	ft_strlcpy(ft_strfind(env, "PWD=") + 4, pwd, ft_strlen(pwd) + 1);
-	return (TRUE);
+	error_msg(chdir(pwd) == 0, "cd ");
+	pwd = getcwd(NULL, 0);
+	/* seg error ft_strlcpy(ft_strfind(env, "PWD=") + 4, pwd, ft_strlen(pwd) + 1); */
+	return (free(pwd), TRUE);
 }
