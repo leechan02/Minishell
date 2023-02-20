025cbe03e86c621e91f1c8f23ced98bd55c9bb56
euiclee <lechan1317@gmail.com>
2023-02-20 14:44:03 +0900

    feat:return

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: euiclee <euiclee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:50:52 by nakoo             #+#    #+#             */
-/*   Updated: 2023/02/20 09:43:25 by euiclee          ###   ########.fr       */
+/*   Updated: 2023/02/20 14:39:34 by euiclee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	only_process(t_tokens *tokens, char **env)
 	{
 		fd = dup(STDOUT_FILENO);
 		find_redir(&tokens[0], 0, 2, 1);
-		exec_builtin(tokens, env);
+		g_exit = !exec_builtin(tokens, env);
 		dup2(fd, STDOUT_FILENO);
 	}
 	else
